43a05e5d1084725f139cef008e661c24865c8dc9
rafif dio fadilah <rafifdiofadilah8@gmail.com>
2024-05-23 16:15:49 +0700

    update

---
+++ html/script.js
@@ -0,0 +1,14 @@
+//ini js 
+function kirim() {
+    var angka1 = document.getElementById("angka1").value;
+    var angka2 = document.getElementById("angka2").value;
+    var hasil = Number(angka1) *  Number(angka2) /2 ;
+    document.getElementById("hasil").innerHTML = hasil ;
+}
+function jumlah() {
+    var sisiA = document.getElementById("sisiA").value;
+    var sisiB = document.getElementById("sisiB").value;
+    var sisiC = document.getElementById("sisiC").value;
+    var total = Number(sisiA) +  Number(sisiB) + Number(sisiC) ;
+    document.getElementById("total").innerHTML = total ;
+}
